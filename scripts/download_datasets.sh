f24995b739269362b357827a77939cd8d7634de7
Davide <davide@trality.com>
2022-09-15 11:39:53 +0200

    update example datasets

--- scripts/download_datasets.sh
+++ scripts/download_datasets.sh
@@ -1,8 +1,10 @@
 mkdir -p datasets/
 
 # Stocks datasets
-python scripts/yfinance_downloader.py --ticker TSLA  --target datasets/tsla_d.csv
+python scripts/yfinance_downloader.py --ticker AAPL  --target datasets/aapl_d.csv
+python scripts/yfinance_downloader.py --ticker SPY  --target datasets/spy_d.csv
 
 # Crypto datasets
 python scripts/binance_downloader.py --interval 1h --symbol BTCUSDT --target datasets/btc_h.csv
 python scripts/binance_downloader.py --interval 1h --symbol ETHUSDT --target datasets/eth_h.csv
+python scripts/binance_downloader.py --interval 1h --symbol XRPUSDT --target datasets/xrp_h.csv
